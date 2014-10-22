38ffd811f9c3085c1bd27fdd41b82c0ba1288312
Gonzalo Díaz <gdiaz@starmount.com>
2014-10-22 10:58:24 -0300

    restauración de gerentes

--- kalos_comun/src/kalos/datos/gerentes/GerenteInterjeccionesImpl.java
+++ kalos_comun/src/kalos/datos/gerentes/GerenteInterjeccionesImpl.java
@@ -10,7 +10,7 @@ import kalos.operaciones.comparadores.ComparadorBeansGriegos;
 
 import org.apache.log4j.Logger;
 
-public class GerenteInterjeccionesImpl implements GerenteInterjecciones{
+public class GerenteInterjeccionesImpl implements GerenteInterjecciones {
 
 	Logger logger=Logger.getLogger(this.getClass().getName());
 	
@@ -19,15 +19,20 @@ public class GerenteInterjeccionesImpl implements GerenteInterjecciones{
 	
 	
 	
-	/**
-	 * @param adverbiosDAO The adverbiosDAO to set.
+	/* (non-Javadoc)
+	 * @see kalos.datos.gerentes.GerenteInterjecciones#setInterjeccionesDAO(kalos.datos.dao.InterjeccionesDAO)
 	 */
+	@Override
 	public void setInterjeccionesDAO(InterjeccionesDAO interjeccionesDAO) {
 		this.interjeccionesDAO = interjeccionesDAO;
 	}
 
 
 
+    /* (non-Javadoc)
+     * @see kalos.datos.gerentes.GerenteInterjecciones#ordena(java.lang.String)
+     */
+    @Override
     public void ordena(String letra){
         List<String> idsetra = interjeccionesDAO.seleccionaPorLetra(letra);
         List<InterjeccionBean> todosLetra=interjeccionesDAO.getRegistros(idsetra);
@@ -44,43 +49,65 @@ public class GerenteInterjeccionesImpl implements GerenteInterjecciones{
      }
     
 
-	/**
-	 * @return Returns the gerenteSignificados.
+	/* (non-Javadoc)
+	 * @see kalos.datos.gerentes.GerenteInterjecciones#getGerenteSignificados()
 	 */
+	@Override
 	public GerenteSignificados getGerenteSignificados() {
 		return gerenteSignificados;
 	}
 
 
-	/**
-	 * @param gerenteSignificados The gerenteSignificados to set.
+	/* (non-Javadoc)
+	 * @see kalos.datos.gerentes.GerenteInterjecciones#setGerenteSignificados(kalos.datos.gerentes.GerenteSignificados)
 	 */
+	@Override
 	public void setGerenteSignificados(GerenteSignificados gerenteSignificados) {
 		this.gerenteSignificados = gerenteSignificados;
 	}
 
 
+	/* (non-Javadoc)
+	 * @see kalos.datos.gerentes.GerenteInterjecciones#seleccionaPorLetra(java.lang.String)
+	 */
+	@Override
 	public List<String> seleccionaPorLetra(String letra) {
 		List<String> ids=interjeccionesDAO.seleccionaPorLetra(letra);
 		return ids;
 	}
 	
+	/* (non-Javadoc)
+	 * @see kalos.datos.gerentes.GerenteInterjecciones#seleccionaPorCanonica(java.lang.String)
+	 */
+	@Override
 	public List<String> seleccionaPorCanonica(String canonica) {
 		List<String> ids=interjeccionesDAO.seleccionaPorCanonica(canonica);
 		return ids;
 	}
 	
+	/* (non-Javadoc)
+	 * @see kalos.datos.gerentes.GerenteInterjecciones#seleccionaPorFormaSinSignificado(java.lang.String)
+	 */
+	@Override
 	public List<InterjeccionBean> seleccionaPorFormaSinSignificado(String forma){
 	    List<InterjeccionBean> resultado=interjeccionesDAO.seleccionaPorFormaSinSignificado(forma);
         return resultado;
 	}
 	
+	/* (non-Javadoc)
+	 * @see kalos.datos.gerentes.GerenteInterjecciones#seleccionaTodos()
+	 */
+	@Override
 	public List<InterjeccionBean> seleccionaTodos(){
         List<InterjeccionBean> resultado=interjeccionesDAO.seleccionaTodos();
         return resultado;
 	}
 	
 
+	/* (non-Javadoc)
+	 * @see kalos.datos.gerentes.GerenteInterjecciones#inserta(kalos.beans.InterjeccionBean)
+	 */
+	@Override
 	public void inserta(InterjeccionBean bean) {
 	    interjeccionesDAO.inserta(bean);
 	    gerenteSignificados.refresca(bean);
@@ -90,22 +117,38 @@ public class GerenteInterjeccionesImpl implements GerenteInterjecciones{
 	
 
 	
+	/* (non-Javadoc)
+	 * @see kalos.datos.gerentes.GerenteInterjecciones#borra(java.lang.String)
+	 */
+	@Override
 	public void borra(String id) {
 	    interjeccionesDAO.borra(id);
 		gerenteSignificados.borraSignificadosDelReferente(id);
 	}
 
+	/* (non-Javadoc)
+	 * @see kalos.datos.gerentes.GerenteInterjecciones#actualiza(kalos.beans.InterjeccionBean)
+	 */
+	@Override
 	public void actualiza(InterjeccionBean bean) {
 	    interjeccionesDAO.modifica(bean);
 		gerenteSignificados.refresca(bean);
 	}
 
+	/* (non-Javadoc)
+	 * @see kalos.datos.gerentes.GerenteInterjecciones#seleccionaUno(java.lang.String)
+	 */
+	@Override
 	@SuppressWarnings("unchecked")
     public InterjeccionBean seleccionaUno(String id) {
 		return interjeccionesDAO.getInidvidual(id);
 	}
 	
 	
+    /* (non-Javadoc)
+     * @see kalos.datos.gerentes.GerenteInterjecciones#getBeans(java.util.List)
+     */
+    @Override
     public List<InterjeccionBean> getBeans(List<String> ids){
     	return interjeccionesDAO.getRegistros(ids);
     }
