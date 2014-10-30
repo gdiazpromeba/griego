61d7e5ef874a0e6af8abcfa00f279959d59163fe
Gonzalo Díaz <gdiaz@starmount.com>
2014-10-30 17:52:03 -0300

    terminando flexion, comenzando con análisis morfológico

--- kalos_comun/src/kalos/flexion/ProveedorDMInterjecciones.java
+++ kalos_comun/src/kalos/flexion/ProveedorDMInterjecciones.java
@@ -2,36 +2,35 @@ package kalos.flexion;
 
 import java.util.Iterator;
 import java.util.List;
-import java.util.Map;
-import javax.swing.table.DefaultTableModel;
-import kalos.B.q;
-import kalos.C.A;
-import kalos.E.E.jA;
-import kalos.G.I;
-import kalos.K.Q;
-import kalos.K.Y;
 
-public class ProveedorDMInterjecciones
-{
-  private jA A;
-  
-  public DefaultTableModel getTMModeloInterjecciones()
-  {
-    DefaultTableModel localDefaultTableModel = new DefaultTableModel(new String[] { "FORMA", "PARTICULARIDAD", "SIGNIFICADO" }, 0);
-    List localList = this.A.seleccionaTodos();
-    Iterator localIterator = localList.iterator();
-    while (localIterator.hasNext())
-    {
-      Y localY = (Y)localIterator.next();
-      Q localQ = (Q)localY.getSignificados().get(q.getEnum(A.getIdiomaSignificados()));
-      String str = localQ.getValor();
-      localDefaultTableModel.addRow(new Object[] { I.strBetaACompleto(localY.getInterjeccion()), localY.getPartic(), str });
+import javax.swing.table.DefaultTableModel;
+
+import kalos.beans.InterjeccionBean;
+import kalos.beans.Significado;
+import kalos.datos.gerentes.GerenteInterjecciones;
+import kalos.enumeraciones.Idioma;
+import kalos.operaciones.OpPalabras;
+import kalos.recursos.Configuracion;
+
+public class ProveedorDMInterjecciones {
+    private GerenteInterjecciones gerenteInterjecciones;
+
+    public DefaultTableModel getTMModeloInterjecciones() {
+	DefaultTableModel localDefaultTableModel = new DefaultTableModel(new String[] { "FORMA", "PARTICULARIDAD",
+		"SIGNIFICADO" }, 0);
+	List<InterjeccionBean> localList = this.gerenteInterjecciones.seleccionaTodos();
+	Iterator<InterjeccionBean> localIterator = localList.iterator();
+	while (localIterator.hasNext()) {
+	    InterjeccionBean localY = localIterator.next();
+	    Significado localQ = localY.getSignificados().get(Idioma.getEnum(Configuracion.getIdiomaSignificados()));
+	    String str = localQ.getValor();
+	    localDefaultTableModel.addRow(new Object[] { OpPalabras.strBetaACompleto(localY.getInterjeccion()),
+		    localY.getPartic(), str });
+	}
+	return localDefaultTableModel;
     }
-    return localDefaultTableModel;
-  }
-  
-  public void setGerenteInterjecciones(jA paramjA)
-  {
-    this.A = paramjA;
-  }
+
+    public void setGerenteInterjecciones(GerenteInterjecciones paramjA) {
+	this.gerenteInterjecciones = paramjA;
+    }
 }
