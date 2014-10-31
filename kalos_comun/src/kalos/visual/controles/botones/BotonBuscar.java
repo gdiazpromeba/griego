65a71de64b1472a37c2ae98d21b0429bd80196ca
Gonzalo Díaz <gdiaz@starmount.com>
2014-10-31 16:25:28 -0300

    algunas cosas de botones

--- kalos_comun/src/kalos/visual/controles/botones/BotonBuscar.java
+++ kalos_comun/src/kalos/visual/controles/botones/BotonBuscar.java
@@ -1,7 +1,44 @@
-{ì"Ëž…´A„1i€ñ(Ã˜xûÀö\›?^pÿyaÎõ1á•[>áh¨¯]h(è“²à½R’(öeÀgpQO( Äª€÷þ]
-·çT|g°µÍ\9ÌÚèž Žˆ.6šç;ä¶%¯Ž=°ñ'¯\¿ÅA)ÇìjD]]žàwu‰qe¾mdÆ’yÏ,ƒ÷Ž5×ÇG0˜µÓsß²úý¬¥3eÏt²ž|/ UUîwÏ,íØË©µ³ÅÍŒBÖ­:2û.£Uf¶)ä…œû¦ç; A¾D’5-¨£|úPž7[¼Ñ	ÙlÊš³Ã—†ÓøàûÕN‡—öÌl
-_1XN0$u˜é÷…Yyë„à#»lŽ›¼¥ø•¥±0×Œá!7á„¢|c?Ëàx¾`„o¼PgÆi\û¥+ß!¨l†.ŠûÄ5þ2¡Œ\ÌN7…-âÊI¶%³ýaþçˆMî-¶åž:¼Í„P¨«ÔÜ×¤Ž2™Oã;ø²:[”òÜ…¬ïàªËÏÏmß‹›ÐîNFígäœbÖÂÂq€˜d—®‡þwÂ4ýë„8þ)g’?ÄúQù|—Æ*¿¬ŸÙ–Ï¤æ*5?(t`yýá`ö·/—ÂœÄ@!þâÊiÜ…#¬ô?ÀZC|e?V©IiÝý?Bn`Iýò±_‘Âæ­ÿ!ÈWP8DÒ
-W.šÅï†ƒÛ?GÊÎÊÀ»”•ü©¾CÆN;Åwà„Œïà}~`®ç”ñË.JˆÂÝÂÂwà÷þÂGc“%+22–ªìe¶•,=BsËCHXÂ&¿$ƒIÁ5pÉß4YìŽ5
-óÛˆÅ¿¤
-X¨a>idû=ßAù}n@	…JHªkï»¼Ø/€3&ñ™+ .ñV6Òwpq™®¡¬¿*;Þâ+c¢˜l5gÍ(/¿ð·…õÂ/¼±»Ð	Ÿ/Ü½	¨mñ‹eÌ$`‡}˜OÂ‰–¨x]'>Ü›Ýò"á€
-ð«î@NGbl¾²Êw(P©Þ<Å¬xž<nõ1^*vÞïŽé=JßÏh¡|ò¹¾ƒ+Î{Åµñ» p˜=`Y})R–ÅÂcˆ¦ø÷Ò]àG]¹{ß!`B…òµ…ˆ@•Ä·~À¾`sÅO†0þæÀ¿ŽñYì"8+
+// Decompiled by Jad v1.5.8g. Copyright 2001 Pavel Kouznetsov.
+// Jad home page: http://www.kpdus.com/jad.html
+// Decompiler options: packimports(3) 
+
+package kalos.visual.controles.botones;
+
+import java.awt.Dimension;
+import java.awt.event.ComponentAdapter;
+import java.awt.event.ComponentEvent;
+import javax.swing.JButton;
+import kalos.C.F;
+import kalos.recursos.Recursos;
+
+public class BotonBuscar extends JButton
+{
+
+    public BotonBuscar()
+    {
+        super(Recursos.cargador.getImagen("buscar16.gif"));
+        setPreferredSize(new Dimension(22, 22));
+        setMaximumSize(new Dimension(22, 22));
+        A();
+    }
+
+    private void A()
+    {
+        addComponentListener(new ComponentAdapter() {
+
+            public void componentResized(ComponentEvent componentevent)
+            {
+                A.setSize(new Dimension(22, 22));
+            }
+
+            final B A;
+
+            
+            {
+                A = B.this;
+                super();
+            }
+        }
+);
+    }
+}
