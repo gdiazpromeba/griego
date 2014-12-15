b73ac4505f86fa4a035319f6ca14b0f27b919e22
Gonzalo Díaz <gdiaz@starmount.com>
2014-12-15 01:19:02 -0300

    no sé

--- kalos_comun/src/kalos/datos/gerentes/GerenteSeguridadImpl.java
+++ kalos_comun/src/kalos/datos/gerentes/GerenteSeguridadImpl.java
@@ -62,7 +62,7 @@ public class GerenteSeguridadImpl implements GerenteSeguridad {
 	 * @param args
 	 */
 	public static void main(String[] args){
-		System.out.println(new GerenteSeguridadImpl().generaClave("Philip Schwartz"));
+		System.out.println(new GerenteSeguridadImpl().generaClave("Stefano Belfiore"));
 	}
 	
 	/**
