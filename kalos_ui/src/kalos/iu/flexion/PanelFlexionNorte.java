e3bfa741416dce3021dae525a6d0b82a720ab478
gdiazpromeba <dongonzalito@gmail.com>
2015-03-31 12:20:31 -0500

    -reinserto PanelFlexionNorte (se había perdido) -issue #20, tomo los archivos jasper de la distribución, estaban corrompidos -arreglo un poco la UtilidadJasper

---
+++ kalos_ui/src/kalos/iu/flexion/PanelFlexionNorte.java
@@ -0,0 +1,194 @@
+// Decompiled by Jad v1.5.8g. Copyright 2001 Pavel Kouznetsov.
+// Jad home page: http://www.kpdus.com/jad.html
+// Decompiler options: packimports(3) 
+
+package kalos.iu.flexion;
+
+import java.awt.BorderLayout;
+
+import javax.swing.JButton;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+
+import kalos.beans.EntradaDiccionario;
+import kalos.enumeraciones.Reportes;
+import kalos.recursos.Configuracion;
+import kalos.recursos.Recursos;
+import kalos.visual.controles.combos.ComboEnumeracion;
+import kalos.visual.controles.deslizador.Deslizador;
+
+import org.apache.log4j.Logger;
+
+import com.jgoodies.forms.builder.PanelBuilder;
+import com.jgoodies.forms.layout.CellConstraints;
+import com.jgoodies.forms.layout.FormLayout;
+
+public class PanelFlexionNorte extends JPanel {
+
+	public JButton getCrear() {
+		return botCrear;
+	}
+
+	public void habilitarBotones() {
+		B = false;
+		botCrear.setEnabled(!B && H);
+		botCrear.setText(Recursos.getCadena("crear"));
+		botCrearMas.setText(Recursos.getCadena("crear"));
+		botCrearMas.setEnabled(true);
+	}
+
+	public ComboEnumeracion getTiposReporte() {
+		return cmbChartTypes;
+	}
+
+	public PanelFlexionNorte() {
+		cmbChartTypes = new ComboEnumeracion("kalos.enumeraciones.Reportes");
+		cmbMoreChartTypes = new ComboEnumeracion(new Reportes[] { Reportes.ARTICULOS_POR_GENERO,
+				Reportes.PRONOMBRES_PERSONALES_POR_CASO,
+				Reportes.PRONOMBRES_PERSONALES_POR_CASO_SIN_DUAL,
+				Reportes.PRONOMBRES_RELATIVOS_POR_GENERO_1,
+				Reportes.PRONOMBRES_RELATIVOS_POR_GENERO_2,
+				Reportes.PRONOMBRES_RELATIVOS_POR_GENERO_3,
+				Reportes.PRONOMBRES_REFLEXIVOS_POR_GENERO,
+				Reportes.PRONOMBRES_INTERROGATIVOS_POR_GENERO, Reportes.CONJUNCIONES_ALFABETICO,
+				Reportes.CONJUNCIONES_POR_TIPO, Reportes.PREPOSICIONES_ALFABETICO,
+				Reportes.PREPOSICIONES_POR_CASO, Reportes.INTERJECCIONES_ALFABETICO,
+				Reportes.PRONOMBRES_INDEFINIDOS_POR_GENERO });
+		botCrear = new JButton(Recursos.getCadena("cargando"));
+		botCrearMas = new JButton(Recursos.getCadena("cargando"));
+		deslizador = new Deslizador();
+		etiqueta = new JLabel();
+		B = true;
+		H = false;
+		F = Logger.getLogger(getClass().getName());
+		etiqueta.setFont(Configuracion.getFont());
+		disposicion();
+		botCrear.setEnabled(false);
+		botCrearMas.setEnabled(false);
+	}
+
+	public void setFormaCanonica(String s) {
+		etiqueta.setText(s);
+	}
+
+	public void disposicion() {
+		FormLayout formlayout = new FormLayout(
+				"pref,3dlu,pref,3dlu,pref, fill:1dlu:grow, right:pref, 3dlu, pref",
+				"fill:pref:grow(.1), 3dlu, fill:pref:grow(.9),  3dlu,pref,3dlu ");
+		PanelBuilder panelbuilder = new PanelBuilder(formlayout);
+		panelbuilder.setDefaultDialogBorder();
+		CellConstraints cellconstraints = new CellConstraints();
+		panelbuilder.addLabel(Recursos.getCadena("forma_canonica"), cellconstraints.xy(1, 1));
+		panelbuilder.add(etiqueta, cellconstraints.xy(3, 1));
+		panelbuilder.addLabel(Recursos.getCadena("elija_tipo_de_reporte"), cellconstraints.xy(1, 3));
+		panelbuilder.add(cmbChartTypes, cellconstraints.xy(3, 3));
+		panelbuilder.add(botCrear, cellconstraints.xy(5, 3));
+		panelbuilder.addLabel(Recursos.getCadena("mas_tipos_de_cuadro"), cellconstraints.xy(1, 5));
+		panelbuilder.add(cmbMoreChartTypes, cellconstraints.xy(3, 5));
+		panelbuilder.add(botCrearMas, cellconstraints.xy(5, 5));
+		panelbuilder.addLabel(Recursos.getCadena("tama\361o_tipografia"), cellconstraints.xy(7, 3));
+		panelbuilder.add(deslizador, cellconstraints.xy(9, 3));
+		setLayout(new BorderLayout());
+		add(panelbuilder.getPanel());
+	}
+
+	public void pueblaSegunEntrada(EntradaDiccionario n) {
+		cmbChartTypes.removeAllItems();
+		if (n == null) {
+			cmbChartTypes.addItem(Reportes.NINGUN_TIPO_DISPONIBLE);
+			H = false;
+		} else {
+			H = true;
+
+			switch (n.getTipoPalabra()) {
+			case Adjetivo: // '\001'
+				cmbChartTypes.addItem(Reportes.ADJETIVOS_POR_GENERO);
+				cmbChartTypes.addItem(Reportes.ADJETIVOS_POR_NUMERO);
+				cmbChartTypes.addItem(Reportes.ADJETIVOS_POR_NUMERO_SIN_DUAL);
+				break;
+
+			case Adverbio: // '\002'
+				cmbChartTypes.addItem(Reportes.NINGUN_TIPO_DISPONIBLE);
+				break;
+
+			case Sustantivo: // '\003'
+				cmbChartTypes.addItem(Reportes.SUSTANTIVOS_POR_NUMERO);
+				cmbChartTypes.addItem(Reportes.SUSTANTIVOS_POR_NUMERO_SIN_DUAL);
+				break;
+
+			case Verbo: // '\004'
+				cmbChartTypes.addItem(Reportes.VERBOS_POR_MODO_ABREVIADO);
+				cmbChartTypes.addItem(Reportes.VERBOS_POR_MODO);
+				cmbChartTypes.addItem(Reportes.VERBOS_POR_VOZ);
+				cmbChartTypes.addItem(Reportes.VERBOS_POR_MODO_SIN_DUAL);
+				cmbChartTypes.addItem(Reportes.VERBOS_POR_VOZ_SIN_DUAL);
+				cmbChartTypes.addItem(Reportes.PARTICIPIOS_POR_CASO_ABREVIADO);
+				cmbChartTypes.addItem(Reportes.PARTICIPIOS_POR_VOZ_ABREVIADO);
+				cmbChartTypes.addItem(Reportes.PARTICIPIOS_POR_NUMERO);
+				cmbChartTypes.addItem(Reportes.PARTICIPIOS_POR_VOZ);
+				cmbChartTypes.addItem(Reportes.PARTICIPIOS_POR_NUMERO_SIN_DUAL);
+				cmbChartTypes.addItem(Reportes.PARTICIPIOS_POR_VOZ_SIN_DUAL);
+				cmbChartTypes.addItem(Reportes.PARTICIPIOS_POR_NUMERO_SIN_VOCATIVO);
+				cmbChartTypes.addItem(Reportes.PARTICIPIOS_POR_VOZ_SIN_VOCATIVO);
+				cmbChartTypes.addItem(Reportes.INFINITIVOS_POR_VOZ);
+				break;
+
+			case Articulo: // '\005'
+				cmbChartTypes.addItem(Reportes.ARTICULOS_POR_GENERO);
+				break;
+
+			case PronombrePersonal: // '\006'
+				cmbChartTypes.addItem(Reportes.PRONOMBRES_PERSONALES_POR_CASO);
+				cmbChartTypes.addItem(Reportes.PRONOMBRES_PERSONALES_POR_CASO_SIN_DUAL);
+				break;
+
+			case PronombreRelativo: // '\007'
+				cmbChartTypes.addItem(Reportes.PRONOMBRES_RELATIVOS_POR_GENERO_1);
+				cmbChartTypes.addItem(Reportes.PRONOMBRES_RELATIVOS_POR_GENERO_2);
+				cmbChartTypes.addItem(Reportes.PRONOMBRES_RELATIVOS_POR_GENERO_3);
+				break;
+
+			case PronombreReflexivo: // '\b'
+				cmbChartTypes.addItem(Reportes.PRONOMBRES_REFLEXIVOS_POR_GENERO);
+				break;
+
+			case Conjuncion: // '\t'
+				cmbChartTypes.addItem(Reportes.CONJUNCIONES_ALFABETICO);
+				cmbChartTypes.addItem(Reportes.CONJUNCIONES_POR_TIPO);
+				break;
+
+			case Preposicion: // '\n'
+				cmbChartTypes.addItem(Reportes.PREPOSICIONES_ALFABETICO);
+				cmbChartTypes.addItem(Reportes.PREPOSICIONES_POR_CASO);
+				break;
+
+			case Interjeccion: // '\013'
+				cmbChartTypes.addItem(Reportes.INTERJECCIONES_ALFABETICO);
+				// fall through
+
+			case PronombreIndefinido: // '\f'
+				cmbChartTypes.addItem(Reportes.PRONOMBRES_INDEFINIDOS_POR_GENERO);
+				break;
+			}
+		}
+		botCrear.setEnabled(!B && H);
+	}
+
+	public JButton getCrearMas() {
+		return botCrearMas;
+	}
+
+	public ComboEnumeracion getMasTipos() {
+		return cmbMoreChartTypes;
+	}
+
+	private ComboEnumeracion cmbChartTypes;
+	private ComboEnumeracion cmbMoreChartTypes;
+	private JButton botCrear;
+	private JButton botCrearMas;
+	Deslizador deslizador;
+	private JLabel etiqueta;
+	private boolean B;
+	private boolean H;
+	Logger F;
+}
--- kalos_ui/src/kalos/iu/jasper/UtilidadJasper.java
+++ kalos_ui/src/kalos/iu/jasper/UtilidadJasper.java
@@ -16,6 +16,7 @@ import kalos.flexion.UtilidadesTM;
 import net.sf.jasperreports.engine.JRException;
 import net.sf.jasperreports.engine.JRRewindableDataSource;
 import net.sf.jasperreports.engine.JasperFillManager;
+import net.sf.jasperreports.engine.JasperPrint;
 import net.sf.jasperreports.engine.JasperReport;
 import net.sf.jasperreports.engine.data.JRTableModelDataSource;
 import net.sf.jasperreports.engine.util.JRLoader;
@@ -28,18 +29,17 @@ import org.apache.log4j.Logger;
 public class UtilidadJasper {
 
 	public void setUtilidadesTM(UtilidadesTM c) {
-		C = c;
+		utildadesTM = c;
 	}
 
 	public UtilidadJasper() {
-		A = Logger.getLogger(getClass().getName());
-		D = new HashMap();
-		B = new HashMap();
-		A();
+		logger = Logger.getLogger(getClass().getName());
+		mapJaspers = new HashMap();
+		mapTiposReporte = new HashMap();
+		pueblaTiposReporte();
 	}
 
-	public static JRRewindableDataSource creaFuenteJR(
-			DefaultTableModel defaulttablemodel) {
+	public static JRRewindableDataSource creaFuenteJR(DefaultTableModel defaulttablemodel) {
 		int i = defaulttablemodel.getRowCount();
 		int j = defaulttablemodel.getColumnCount();
 		for (int k = 0; k < i; k++) {
@@ -57,103 +57,91 @@ public class UtilidadJasper {
 		return c;
 	}
 
-	public JRViewerKalos visorJasper(Reportes m, Map map, DefaultTableModel defaulttablemodel) {
-		String s = (new StringBuilder()).append("armado_reportes/")
-				.append(A(m, C.nombresColumna(defaulttablemodel))).toString();
+	public JRViewerKalos visorJasper(Reportes tipoReporte, Map map, DefaultTableModel defaulttablemodel) {
+		String nombreArchivoReporte = getArchivoReporte(tipoReporte, utildadesTM.nombresColumna(defaulttablemodel));
+	    String pathArchivoReporte = "armado_reportes/" + nombreArchivoReporte;
 		try {
-			JasperReport jasperreport = (JasperReport) D.get(s);
+			JasperReport jasperreport = (JasperReport) mapJaspers.get(pathArchivoReporte);
 			if (jasperreport == null) {
-				jasperreport = (JasperReport) JRLoader.loadObject(s);
-				D.put(s, jasperreport);
+				jasperreport = (JasperReport) JRLoader.loadObject(pathArchivoReporte);
+				mapJaspers.put(pathArchivoReporte, jasperreport);
 			}
-			net.sf.jasperreports.engine.JasperPrint jasperprint = JasperFillManager
-					.fillReport(jasperreport, map, new JRTableModelDataSource(
-							defaulttablemodel));
+			JasperPrint jasperprint = JasperFillManager.fillReport(jasperreport, map, new JRTableModelDataSource(defaulttablemodel));
 			JRViewerKalos a = new JRViewerKalos(jasperprint);
 			return a;
 		} catch (JRException jrexception) {
-			A.error("error al crear el visor de jasper", jrexception);
+			logger.error("error al crear el visor de jasper", jrexception);
 		}
 		return null;
 	}
 
-	private void A() {
-		B.put(Reportes.VERBOS_POR_MODO, "verbos_completo.jasper");
-		B.put(Reportes.VERBOS_POR_MODO_SIN_DUAL, "verbos_completo.jasper");
-		B.put(Reportes.VERBOS_POR_MODO_ABREVIADO, "verbos_compacto.jasper");
-		B.put(Reportes.VERBOS_POR_VOZ, "verbos_voz_primero.jasper");
-		B.put(Reportes.VERBOS_POR_VOZ_SIN_DUAL, "verbos_voz_primero.jasper");
-		B.put(Reportes.INFINITIVOS_POR_VOZ, "infinitivos.jasper");
-		B.put(Reportes.PARTICIPIOS_POR_CASO_ABREVIADO, "participios_nom_gen.jasper");
-		B.put(Reportes.PARTICIPIOS_POR_VOZ_ABREVIADO, "participios_nom_gen_voz_primero.jasper");
-		B.put(Reportes.PARTICIPIOS_POR_NUMERO, "participios_completo.jasper");
-		B.put(Reportes.PARTICIPIOS_POR_VOZ, "participios_voz_primero.jasper");
-		B.put(Reportes.PARTICIPIOS_POR_NUMERO_SIN_DUAL, "participios_sin_dual.jasper");
-		B.put(Reportes.PARTICIPIOS_POR_VOZ_SIN_DUAL, "participios_voz_primero.jasper");
-		B.put(Reportes.PARTICIPIOS_POR_NUMERO_SIN_VOCATIVO, "participios_completo.jasper");
-		B.put(Reportes.PARTICIPIOS_POR_VOZ_SIN_VOCATIVO, "participios_voz_primero.jasper");
-		B.put(Reportes.SUSTANTIVOS_POR_NUMERO, "sustantivos_completo.jasper");
-		B.put(Reportes.SUSTANTIVOS_POR_NUMERO_SIN_DUAL, "sustantivos_sin_dual.jasper");
-		B.put(Reportes.ADJETIVOS_POR_NUMERO, "adjetivos_por_numero.jasper");
-		B.put(Reportes.ADJETIVOS_POR_NUMERO_SIN_DUAL, "adjetivos_por_numero_sin_dual.jasper");
-		B.put(Reportes.ARTICULOS_POR_GENERO, "articulos_masc_fem_neu.jasper");
-		B.put(Reportes.PRONOMBRES_PERSONALES_POR_CASO, "pronombres_personales_por_caso.jasper");
-		B.put(Reportes.PRONOMBRES_PERSONALES_POR_CASO_SIN_DUAL, "pronombres_personales_por_caso.jasper");
-		B.put(Reportes.PRONOMBRES_RELATIVOS_POR_GENERO_1, "pronombres_relativos_por_genero.jasper");
-		B.put(Reportes.PRONOMBRES_RELATIVOS_POR_GENERO_2, "pronombres_relativos_por_genero.jasper");
-		B.put(Reportes.PRONOMBRES_RELATIVOS_POR_GENERO_3, "pronombres_relativos_por_genero.jasper");
-		B.put(Reportes.PRONOMBRES_REFLEXIVOS_POR_GENERO, "pronombres_reflexivos_por_genero.jasper");
-		B.put(Reportes.PRONOMBRES_INTERROGATIVOS_POR_GENERO, "pronombres_interrogativos_por_genero.jasper");
-		B.put(Reportes.CONJUNCIONES_POR_TIPO, "conjunciones_por_tipo.jasper");
-		B.put(Reportes.CONJUNCIONES_ALFABETICO, "conjunciones_alfabeticamente.jasper");
-		B.put(Reportes.PREPOSICIONES_ALFABETICO, "preposiciones_alfabeticamente.jasper");
-		B.put(Reportes.PREPOSICIONES_POR_CASO, "preposiciones_por_caso.jasper");
-		B.put(Reportes.INTERJECCIONES_ALFABETICO, "interjecciones_alfabeticamente.jasper");
-		B.put(Reportes.PRONOMBRES_INDEFINIDOS_POR_GENERO, "pronombres_indefinidos_por_genero.jasper");
+	private void pueblaTiposReporte() {
+		mapTiposReporte.put(Reportes.VERBOS_POR_MODO, "verbos_completo.jasper");
+		mapTiposReporte.put(Reportes.VERBOS_POR_MODO_SIN_DUAL, "verbos_completo.jasper");
+		mapTiposReporte.put(Reportes.VERBOS_POR_MODO_ABREVIADO, "verbos_compacto.jasper");
+		mapTiposReporte.put(Reportes.VERBOS_POR_VOZ, "verbos_voz_primero.jasper");
+		mapTiposReporte.put(Reportes.VERBOS_POR_VOZ_SIN_DUAL, "verbos_voz_primero.jasper");
+		mapTiposReporte.put(Reportes.INFINITIVOS_POR_VOZ, "infinitivos.jasper");
+		mapTiposReporte.put(Reportes.PARTICIPIOS_POR_CASO_ABREVIADO, "participios_nom_gen.jasper");
+		mapTiposReporte.put(Reportes.PARTICIPIOS_POR_VOZ_ABREVIADO, "participios_nom_gen_voz_primero.jasper");
+		mapTiposReporte.put(Reportes.PARTICIPIOS_POR_NUMERO, "participios_completo.jasper");
+		mapTiposReporte.put(Reportes.PARTICIPIOS_POR_VOZ, "participios_voz_primero.jasper");
+		mapTiposReporte.put(Reportes.PARTICIPIOS_POR_NUMERO_SIN_DUAL, "participios_sin_dual.jasper");
+		mapTiposReporte.put(Reportes.PARTICIPIOS_POR_VOZ_SIN_DUAL, "participios_voz_primero.jasper");
+		mapTiposReporte.put(Reportes.PARTICIPIOS_POR_NUMERO_SIN_VOCATIVO, "participios_completo.jasper");
+		mapTiposReporte.put(Reportes.PARTICIPIOS_POR_VOZ_SIN_VOCATIVO, "participios_voz_primero.jasper");
+		mapTiposReporte.put(Reportes.SUSTANTIVOS_POR_NUMERO, "sustantivos_completo.jasper");
+		mapTiposReporte.put(Reportes.SUSTANTIVOS_POR_NUMERO_SIN_DUAL, "sustantivos_sin_dual.jasper");
+		mapTiposReporte.put(Reportes.ADJETIVOS_POR_NUMERO, "adjetivos_por_numero.jasper");
+		mapTiposReporte.put(Reportes.ADJETIVOS_POR_NUMERO_SIN_DUAL, "adjetivos_por_numero_sin_dual.jasper");
+		mapTiposReporte.put(Reportes.ARTICULOS_POR_GENERO, "articulos_masc_fem_neu.jasper");
+		mapTiposReporte.put(Reportes.PRONOMBRES_PERSONALES_POR_CASO, "pronombres_personales_por_caso.jasper");
+		mapTiposReporte.put(Reportes.PRONOMBRES_PERSONALES_POR_CASO_SIN_DUAL, "pronombres_personales_por_caso.jasper");
+		mapTiposReporte.put(Reportes.PRONOMBRES_RELATIVOS_POR_GENERO_1, "pronombres_relativos_por_genero.jasper");
+		mapTiposReporte.put(Reportes.PRONOMBRES_RELATIVOS_POR_GENERO_2, "pronombres_relativos_por_genero.jasper");
+		mapTiposReporte.put(Reportes.PRONOMBRES_RELATIVOS_POR_GENERO_3, "pronombres_relativos_por_genero.jasper");
+		mapTiposReporte.put(Reportes.PRONOMBRES_REFLEXIVOS_POR_GENERO, "pronombres_reflexivos_por_genero.jasper");
+		mapTiposReporte.put(Reportes.PRONOMBRES_INTERROGATIVOS_POR_GENERO, "pronombres_interrogativos_por_genero.jasper");
+		mapTiposReporte.put(Reportes.CONJUNCIONES_POR_TIPO, "conjunciones_por_tipo.jasper");
+		mapTiposReporte.put(Reportes.CONJUNCIONES_ALFABETICO, "conjunciones_alfabeticamente.jasper");
+		mapTiposReporte.put(Reportes.PREPOSICIONES_ALFABETICO, "preposiciones_alfabeticamente.jasper");
+		mapTiposReporte.put(Reportes.PREPOSICIONES_POR_CASO, "preposiciones_por_caso.jasper");
+		mapTiposReporte.put(Reportes.INTERJECCIONES_ALFABETICO, "interjecciones_alfabeticamente.jasper");
+		mapTiposReporte.put(Reportes.PRONOMBRES_INDEFINIDOS_POR_GENERO, "pronombres_indefinidos_por_genero.jasper");
 	}
 
-	private String A(Reportes m, List list) {
-		String s = (String) B.get(m);
-		if (s != null)
-			return s;
-		if (m.equals(m.ADJETIVOS_POR_GENERO)) {
-			boolean flag = list.contains("MASCULINO");
-			boolean flag1 = list.contains("FEMENINO");
-			boolean flag2 = list.contains("NEUTRO");
-			boolean flag3 = list.contains("MASC_FEM");
-			if (flag && flag1 && flag2 && !flag3)
+	private String getArchivoReporte(Reportes tipoReporte, List<String> nombresColumna) {
+		String archivoReporte = mapTiposReporte.get(tipoReporte);
+		if (archivoReporte != null)
+			return archivoReporte;
+		if (tipoReporte.equals(Reportes.ADJETIVOS_POR_GENERO)) {
+			boolean tieneMasc = nombresColumna.contains("MASCULINO");
+			boolean tieneFem = nombresColumna.contains("FEMENINO");
+			boolean tieneNeu = nombresColumna.contains("NEUTRO");
+			boolean tieneMascFem = nombresColumna.contains("MASC_FEM");
+			if (tieneMasc && tieneFem && tieneNeu && !tieneMascFem)
 				return "adjetivos_masc_fem_neu.jasper";
-			if (!flag && !flag1 && flag2 && flag3)
+			if (!tieneMasc && !tieneFem && tieneNeu && tieneMascFem)
 				return "adjetivos_mascfem_neu.jasper";
-			if (flag && flag1 && flag2 && flag3) {
+			if (tieneMasc && tieneFem && tieneNeu && tieneMascFem) {
 				return "adjetivos_mascfem_masc_fem_neu.jasper";
 			} else {
-				StringBuffer stringbuffer = new StringBuffer(200);
-				stringbuffer
-						.append("esa combinaci\363n de columnas no est\341 contemplada para los reportes de adjetivos \n");
-				stringbuffer.append((new StringBuilder())
-						.append(" masc existe=").append(flag).append("\n")
-						.toString());
-				stringbuffer.append((new StringBuilder())
-						.append(" fem existe=").append(flag1).append("\n")
-						.toString());
-				stringbuffer.append((new StringBuilder())
-						.append(" neu existe=").append(flag2).append("\n")
-						.toString());
-				stringbuffer.append((new StringBuilder())
-						.append(" mascfem existe=").append(flag3).append("\n")
-						.toString());
-				A.error(stringbuffer.toString());
-				throw new RuntimeException(stringbuffer.toString());
+				StringBuffer sb = new StringBuffer(200);
+				sb.append("esa combinación de columnas no está contemplada para los reportes de adjetivos \n");
+				sb.append(" masc existe=").append(tieneMasc).append("\n");
+				sb.append(" fem existe=").append(tieneFem).append("\n");
+				sb.append(" neu existe=").append(tieneNeu).append("\n");
+				sb.append(" mascfem existe=").append(tieneMascFem).append("\n");
+				logger.error(sb.toString());
+				throw new RuntimeException(sb.toString());
 			}
 		} else {
-			throw new RuntimeException(
-					"ese tipo de reporte no est\341 contemplado");
+			throw new RuntimeException("ese tipo de reporte no está contemplado");
 		}
 	}
 
-	private Logger A;
-	private UtilidadesTM C;
-	private Map D;
-	private Map B;
+	private Logger logger;
+	private UtilidadesTM utildadesTM;
+	private Map<String, JasperReport> mapJaspers;
+	private Map<Reportes, String> mapTiposReporte;
 }
